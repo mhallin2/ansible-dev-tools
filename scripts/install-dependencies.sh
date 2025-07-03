c9fcd63dcc02853952ef9a5b955bc0953b65bf09
mhallin2 <mhallin2@volvocars.com>
2025-07-03 13:17:28 +0000

    feat: update dependencies and configurations in devcontainer and requirements files

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,3 +2,5 @@ git config --global user.name mhallin2
 git config --global user.email mhallin2@volvocars.com
 mkdir -p /workspaces/github/mhallin2
 #git clone https://github.com/mhallin2/Hosting-Ansible-Playbooks /workspaces/github/mhallin2/Hosting-Ansible-Playbooks
+cd /workspaces/github/mhallin2/Hosting-Ansible-Playbooks
+ansible-galaxy collection install -r requirements.yml
