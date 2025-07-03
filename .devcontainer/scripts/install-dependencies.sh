06a25eb98683f6dcaa78787957a7699d6ec062ce
mhallin2 <mhallin2@volvocars.com>
2025-07-03 12:47:37 +0000

    feat: update devcontainer and scripts for user configuration and dependencies

---
+++ .devcontainer/scripts/install-dependencies.sh
@@ -0,0 +1,4 @@
+git config --global user.name mhallin2
+git config --global user.email mhallin2@volvocars.com
+mkdir -p /workspaces/github/mhallin2
+git clone https://github.com/mhallin2/Hosting-Ansible-Playbooks /workspaces/github/mhallin2/Hosting-Ansible-Playbooks
