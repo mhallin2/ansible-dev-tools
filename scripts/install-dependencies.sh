8e953280a1659d5149f624b13b6dd3bd34065c84
mhallin2 <mhallin2@volvocars.com>
2025-07-03 12:58:55 +0000

    yu

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,4 +1,4 @@
 git config --global user.name mhallin2
 git config --global user.email mhallin2@volvocars.com
 mkdir -p /workspaces/github/mhallin2
-git clone https://github.com/mhallin2/Hosting-Ansible-Playbooks /workspaces/github/mhallin2/Hosting-Ansible-Playbooks
+#git clone https://github.com/mhallin2/Hosting-Ansible-Playbooks /workspaces/github/mhallin2/Hosting-Ansible-Playbooks
