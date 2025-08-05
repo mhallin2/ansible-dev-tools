76f7c873d0f73db3aecebbda4c0701f104caa578
mhallin2 <mhallin2@volvocars.com>
2025-08-05 08:11:33 +0000

    feat: ensure script is executable and enable git clone command

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 git config --global user.name mhallin2
 git config --global user.email mhallin2@volvocars.com
 mkdir -p /workspaces/github/mhallin2
@@ -7,6 +8,6 @@ pip3 install -r ~/.ansible/collections/ansible_collections/azure/azcollection/re
 # azd auth login
 dnf install azure-cli
 az login
-#git clone https://github.com/mhallin2/Hosting-Ansible-Playbooks /workspaces/github/mhallin2/Hosting-Ansible-Playbooks
+git clone https://github.com/mhallin2/Hosting-Ansible-Playbooks /workspaces/github/mhallin2/Hosting-Ansible-Playbooks
 #cd /workspaces/github/mhallin2/Hosting-Ansible-Playbooks
 #ansible-galaxy collection install -r requirements.yml
