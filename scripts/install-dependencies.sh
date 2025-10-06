de37bc527fecf4de5325060707149e0e610e7e9b
mhallin2 <mhallin2@volvocars.com>
2025-10-06 08:11:20 +0000

    fix: add git clone for Hosting-Ansible-EE in install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -16,6 +16,7 @@ az config set core.login_experience_v2=off
 az login
 git clone https://github.com/mhallin2/Hosting-Ansible-Playbooks /workspaces/github/mhallin2/Hosting-Ansible-Playbooks
 git clone https://github.com/mhallin2/Hosting-Ansible-Collections /workspaces/github/mhallin2/Hosting-Ansible-Collections
+git clone https://github.com/mhallin2/Hosting-Ansible-EE.git /workspaces/github/mhallin2/Hosting-Ansible-EE
 /bin/bash "/workspaces/ansible-dev-tools/scripts/update-ansible-config.sh"
 ansible-galaxy collection install -r /workspaces/github/mhallin2/Hosting-Ansible-Playbooks/requirements.yml
 ansible-galaxy collection install /workspaces/github/mhallin2/Hosting-Ansible-Playbooks/collections/hosting_internal
