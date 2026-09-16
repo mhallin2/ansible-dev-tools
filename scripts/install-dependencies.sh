31ccc3ca4d28350f3b44f290c3d905fda934e08b
mhallin2 <mhallin2@volvocars.com>
2026-09-16 05:33:49 +0000

    fix: comment out az login command in install-dependencies script

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -18,7 +18,7 @@ pip install azure-cli==2.76
 curl -fsSL https://aka.ms/install-azd.sh | bash
 azd auth login
 az config set core.login_experience_v2=off
-az login
+# az login
 
 # Install/Configure user related settings and dependencies
 /bin/bash "$WORKSPACE/scripts/setup-extras.sh"
